2c05af02a99d1f0aad0a35dbb514c917c505bb65
longzekai <longzekai@sina.com>
2018-05-11 18:09:51 +0800

    目录结构调整，将demo 分类整理。

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,20 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MainApp());
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return new MaterialApp(
-      title: 'Startup Name Generator',
-//      theme: new ThemeData(
-//        primaryColor: Colors.white,
-//      ),
-      home: new RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-
-  final _saved = new Set<WordPair>();
-
-  final TextStyle _biggerFont = new TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    void _pushSaved() {
-      Navigator.of(context).push(
-        new MaterialPageRoute(
-          builder: (context) {
-            final tiles = _saved.map(
-              (pair) {
-                return new ListTile(
-                  title: new Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              },
-            );
-            final divided = ListTile
-                .divideTiles(
-                  context: context,
-                  tiles: tiles,
-                )
-                .toList();
-            return new Scaffold(
-              appBar: new AppBar(
-                title: new Text('Saved Suggestions'),
-              ),
-              body: new ListView(children: divided,),
-            );
-          },
+      home:  new Scaffold(
+        appBar:  new AppBar(
+          title: new Text('Flutter Demo合集'),
         ),
-      );
-    }
 
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
-        ],
+        body: new Center(child: new Text('test')),
       ),
-      body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
 }
